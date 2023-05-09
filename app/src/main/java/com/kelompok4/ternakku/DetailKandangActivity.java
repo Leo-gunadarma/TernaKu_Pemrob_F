038034d0ca2a2262fd8aee78fdee059f541e4e5c
Leo Gunadarma <67279463+Leo-gunadarma@users.noreply.github.com>
2023-05-09 10:36:51 +0800

    Menambahkan detail activity

---
+++ app/src/main/java/com/kelompok4/ternakku/DetailKandangActivity.java
@@ -0,0 +1,14 @@
+package com.kelompok4.ternakku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailKandangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_kandang);
+    }
+}
